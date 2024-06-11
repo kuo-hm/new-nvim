025d4e463beeabf1c27134027b091496a11c831c
Hmoura oussama <hmouraoussama@gmail.com>
2024-06-11 22:49:39 +0100

    Added new ai plugin

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -15,6 +15,25 @@ return {
 
   -- disable trouble
   {"andweeb/presence.nvim"},
+   {
+      "supermaven-inc/supermaven-nvim",
+      config = function()
+        require("supermaven-nvim").setup({})
+      end,
+      keymaps = {
+    accept_suggestion = "<Tab>",
+    clear_suggestion = "<C-]>",
+    accept_word = "<C-j>",
+  },
+  ignore_filetypes = { cpp = true },
+  color = {
+    suggestion_color = "#ffffff",
+    cterm = 244,
+  },
+  disable_inline_completion = false, -- disables inline completion for use with cmp
+  disable_keymaps = false -- disables built in keymaps for more manual control
+    }
+  ,
   -- override nvim-cmp and add cmp-emoji
   {
     "hrsh7th/nvim-cmp",
